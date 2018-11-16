a9b4f38bf2225f8bd5af44029d33e37b135b4cc1
Sudhakar.Badugu <sudhakar.badugu@asg.com>
2018-11-16 08:29:12 +0530

    while and dowhile

--- programs/basics/SwitchExample2.java
+++ programs/basics/SwitchExample2.java
@@ -1,17 +1,30 @@
-public class SwitchExample2{
-     public static void main(String a[]){
-	int books=2;
-	 
-	 if(books==1){
-		 System.out.println("telugu");
+class Switch {
+ public static void main(String...q) {
+     
+     int age = 30;
+	 String s="2";
+	  
+	 switch(s) {
+	     case "1": {
+		      System.out.println( 18);
+			  break;
+		 }
+		 case  "2": {
+		      System.out.println( 30);
+			  break;
+		 }
+		 case "3": {
+		     System.out.println( 29);
+			  break;
+		 }
+		 case "4": {
+		    System.out.println(20);
+			  break;
+		 }
+		 default: {
+		    System.out.println(35);
+			  
+		 }
 	 }
-	 else if(books==2){
-		 System.out.println("hindi");
-	 }
-	 else if(books==3){
-		 System.out.println("english");
-	 }
-	 
-	 }
-	 }
+ }
 }
