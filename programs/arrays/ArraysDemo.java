01d307fc110a6b9302750b621d78c70e55f28d89
Murali <sudhakar0143@gmail.com>
2019-01-28 08:37:19 +0530

    arrays

--- programs/arrays/ArraysDemo.java
+++ programs/arrays/ArraysDemo.java
@@ -2,25 +2,34 @@ class ArraysDemo {
 	public static void main(String[] args){
 		
 		//declaration
-		byte[] a = new byte[5];
+		int[] a = new int[5];
 		
 		// value initilization.
-		a[0] = 1;//"s";//ce
-		a[1] = 2;
-		a[2] = 3;
-		a[3] = 4;
-		a[4] = 5;
-		//a[5] = 5; Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: 5
-        //at ArraysDemo.main(ArraysDemo.java:12)
+		a[0] = 100;
+		a[2] = 200;
+		a[1] = 300;
+		a[3] = 400;
+		a[4] = 500;
+		a[4] = 350;
+		// a[5] = 600; //ArrayIndexOutOfBoundsException
 		
 		System.out.println(a);
 		
 		
 		//value printing
 		System.out.println(a[0]);
-		System.out.println(a[4]);
+		// System.out.println(a[1]);
+		// System.out.println(a[2]);
+		// System.out.println(a[3]);
+		// System.out.println(a[4]);
 		
-		for(int i = 0; i< a.length ; i++){
+
+		for(int i = 0; i < a.length ;i++){
+			System.out.println(a[i]);
+		}
+
+		System.out.println("reverse array");
+		for(int i = a.length - 1; i >=0  ;i--){
 			System.out.println(a[i]);
 		}
 		
@@ -28,9 +37,26 @@ class ArraysDemo {
 		names[0] ="Sudhakar";
 		names[1] = "venky";
 		names[3] = "murali";
+		for(String name : names){
+			System.out.println(name);
+		}
 		
 		for(int j = 0; j < names.length ; j++){
 			System.out.println(names[j]);
 		}
+		
+		int[] marks = {60, 60, 70, 35, 90, 54}; //declaration and initilization
+		for(int k = 0; k < marks.length; k++){
+			System.out.println(marks[k]);
+		}
+		
+		
+		//for each only for arrays and collections
+		for(int x: marks){
+			System.out.println(x);
+		}
+		// int[] abc;
+		// abc = {34,22};	//CE
+		
 	}
 }
