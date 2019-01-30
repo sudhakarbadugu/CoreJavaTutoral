e34d4b3142402661d3c85427079e84f72e8cea3c
Murali <sudhakar0143@gmail.com>
2019-01-30 15:18:19 +0530

    collection

---
+++ programs/collections/ArrayList1.java
@@ -0,0 +1,12 @@
+import java.util.*; 
+  public class ArrayList1 {
+	
+	public static void main(String[] args){
+	List l = new ArrayList();
+		l.add(42);
+		l.add(32);
+		System.out.println(l);
+		l.add(0,23);
+		System.out.println(l);
+	 }
+}
--- programs/collections/ArrayListDemo.java
+++ programs/collections/ArrayListDemo.java
@@ -5,29 +5,29 @@ public class ArrayListDemo {
 		List l = new ArrayList();
 		l.add(60);
 		l.add(90);
-		l.add(60);
-		l.add(70);
-		l.add(56);
-		l.add(45);
-		l.add(45);
-		l.add(45);
-		l.add(45);
-		l.add(34);
+		 l.add(60);
+		 l.add(70);
+		 l.add(56);
+		 l.add(45);
+	 l.add(45);
+		 l.add(45);
+		 l.add(45);
+		 l.add(34);
 		
 		l.add(11);
 		System.out.println(l);
-		l.add(0, 1);
+		 l.add(0, 1);
 		
-		System.out.println(l);
-		System.out.println(l.get(3));
-		System.out.println(l.size());
-		System.out.println(l.isEmpty());
+		 System.out.println(l);
+		 System.out.println(l.get(3));
+		 System.out.println(l.size());
+		 System.out.println(l.isEmpty());
 		
-		l.set(0, 90);
-		System.out.println(l);
+		 l.set(0, 90);
+		 System.out.println(l);
 		
 		List l2 = new ArrayList();
-		l2.addAll(l);
-	System.out.println(l2);
+		 l2.addAll(l);
+	 System.out.println(l2);
 	}
 }
